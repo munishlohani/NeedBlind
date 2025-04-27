d2bacb770227ff6f56e97367d47c42c729fe4762
Munish Lohani <munish.lohani@gmail.com>
2025-04-27 10:34:18 +0800

    Header Change

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
     <>
       <Nav />
       <div className="container mx-auto px-4 py-8">
-        <h1 className="text-3xl font-bold text-center mb-8 text-purple-700">Medical Image Analysis</h1>
+        <h1 className="text-3xl font-bold text-center mb-8 text-purple-700 ">DocC- Your Health Companion</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {uploadOptions.map((option, index) => (
             <div
